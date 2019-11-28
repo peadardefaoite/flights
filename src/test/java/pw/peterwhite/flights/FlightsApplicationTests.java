143c5fde7683f60b317b9c4dad7fc38a852375cc
Peter White <james.peter.white4@gmail.com>
2019-11-28 19:24:09 +0000

    Skeleton service for Flight

---
+++ src/test/java/pw/peterwhite/flights/FlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package pw.peterwhite.flights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
